c652f80d0294af8b9270a7c9b16375cb40c0756b
Benitoite <kd6kxr@gmail.com>
2018-11-29 19:37:42 -0800

    Create xslconfig.sh

---
+++ xslconfig.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#  xslconfig.sh
+#  
+#
+#  Created by Richard Barber on 11/29/18.
+#  based on http://www.linuxfromscratch.org/blfs/view/svn/pst/docbook-xsl.html
+if [ ! -d /etc/xml ]; then install -v -m755 -d /etc/xml; fi &&
+if [ ! -f /etc/xml/catalog ]; then
+xmlcatalog --noout --create /etc/xml/catalog
+fi &&
+
+xmlcatalog --noout --add "rewriteSystem" \
+"http://docbook.sourceforge.net/release/xsl/1.79.2" \
+"/usr/share/xml/docbook/xsl-stylesheets-1.79.2" \
+/etc/xml/catalog &&
+
+xmlcatalog --noout --add "rewriteURI" \
+"http://docbook.sourceforge.net/release/xsl/1.79.2" \
+"/usr/share/xml/docbook/xsl-stylesheets-1.79.2" \
+/etc/xml/catalog &&
+
+xmlcatalog --noout --add "rewriteSystem" \
+"http://docbook.sourceforge.net/release/xsl/current" \
+"/usr/share/xml/docbook/xsl-stylesheets-1.79.2" \
+/etc/xml/catalog &&
+
+xmlcatalog --noout --add "rewriteURI" \
+"http://docbook.sourceforge.net/release/xsl/current" \
+"/usr/share/xml/docbook/xsl-stylesheets-1.79.2" \
+/etc/xml/catalog
