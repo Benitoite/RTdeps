424cb04df5e76b688dbca3b6ebb668affeb987e3
Benitoite <kd6kxr@gmail.com>
2021-01-02 21:03:07 -0800

    Update docbook-script1.sh

--- docbook-script1.sh
+++ docbook-script1.sh
@@ -5,10 +5,10 @@
 #  Adapted from http://www.linuxfromscratch.org/blfs/view/cvs/pst/docbook.html
 #  Created by Richard Barber on 11/28/18.
 #  
-install -v -d -m755 /usr/local/share/xml/docbook/xml-dtd-4.3 && \
+install -v -d -m755 /opt/local/share/xml/docbook/xml-dtd-4.3 && \
 install -v -d -m755 /etc/xml && \
 cp -v -af docbook.cat *.dtd ent/ *.mod \
-/usr/local/share/xml/docbook/xml-dtd-4.3
+/opt/local/share/xml/docbook/xml-dtd-4.3
 
 # Create (or update) and populate the /etc/xml/docbook catalog file by running the following commands as the root user:
 
@@ -21,35 +21,35 @@ xmlcatalog --noout --add "public" \
 /etc/xml/docbook &&
 xmlcatalog --noout --add "public" \
 "-//OASIS//DTD DocBook XML CALS Table Model V4.3//EN" \
-"file:///usr/local/share/xml/docbook/xml-dtd-4.3/calstblx.dtd" \
+"file:///opt/local/share/xml/docbook/xml-dtd-4.3/calstblx.dtd" \
 /etc/xml/docbook &&
 xmlcatalog --noout --add "public" \
 "-//OASIS//DTD XML Exchange Table Model 19990315//EN" \
-"file:///usr/local/share/xml/docbook/xml-dtd-4.3/soextblx.dtd" \
+"file:///opt/local/share/xml/docbook/xml-dtd-4.3/soextblx.dtd" \
 /etc/xml/docbook &&
 xmlcatalog --noout --add "public" \
 "-//OASIS//ELEMENTS DocBook XML Information Pool V4.3//EN" \
-"file:///usr/local/share/xml/docbook/xml-dtd-4.3/dbpoolx.mod" \
+"file:///opt/local/share/xml/docbook/xml-dtd-4.3/dbpoolx.mod" \
 /etc/xml/docbook &&
 xmlcatalog --noout --add "public" \
 "-//OASIS//ELEMENTS DocBook XML Document Hierarchy V4.3//EN" \
-"file:///usr/local/share/xml/docbook/xml-dtd-4.3/dbhierx.mod" \
+"file:///opt/local/share/xml/docbook/xml-dtd-4.3/dbhierx.mod" \
 /etc/xml/docbook &&
 xmlcatalog --noout --add "public" \
 "-//OASIS//ELEMENTS DocBook XML HTML Tables V4.3//EN" \
-"file:///usr/local/share/xml/docbook/xml-dtd-4.3/htmltblx.mod" \
+"file:///opt/local/share/xml/docbook/xml-dtd-4.3/htmltblx.mod" \
 /etc/xml/docbook &&
 xmlcatalog --noout --add "public" \
 "-//OASIS//ENTITIES DocBook XML Notations V4.3//EN" \
-"file:///usr/local/share/xml/docbook/xml-dtd-4.3/dbnotnx.mod" \
+"file:///opt/local/share/xml/docbook/xml-dtd-4.3/dbnotnx.mod" \
 /etc/xml/docbook &&
 xmlcatalog --noout --add "public" \
 "-//OASIS//ENTITIES DocBook XML Character Entities V4.3//EN" \
-"file:///usr/local/share/xml/docbook/xml-dtd-4.3/dbcentx.mod" \
+"file:///opt/local/share/xml/docbook/xml-dtd-4.3/dbcentx.mod" \
 /etc/xml/docbook &&
 xmlcatalog --noout --add "public" \
 "-//OASIS//ENTITIES DocBook XML Additional General Entities V4.3//EN" \
-"file:///usr/local/share/xml/docbook/xml-dtd-4.3/dbgenent.mod" \
+"file:///opt/local/share/xml/docbook/xml-dtd-4.3/dbgenent.mod" \
 /etc/xml/docbook &&
 xmlcatalog --noout --add "rewriteSystem" \
 "http://www.oasis-open.org/docbook/xml/4.3" \
@@ -57,7 +57,7 @@ xmlcatalog --noout --add "rewriteSystem" \
 /etc/xml/docbook &&
 xmlcatalog --noout --add "rewriteURI" \
 "http://www.oasis-open.org/docbook/xml/4.3" \
-"file:///usr/local/share/xml/docbook/xml-dtd-4.3" \
+"file:///opt/local/share/xml/docbook/xml-dtd-4.3" \
 /etc/xml/docbook
 
 # Create (or update) and populate the /etc/xml/catalog catalog file by running the following commands as the root user:
